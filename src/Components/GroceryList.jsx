e5ce2289a8169c463e87d316c5ea62283e1c82e9
shiva-69 <92380211+shiva-69@users.noreply.github.com>
2022-06-09 16:18:29 +0530

    commit1

--- src/Components/GroceryList.jsx
+++ src/Components/GroceryList.jsx
@@ -5,10 +5,10 @@ const GroceryList = ({list, handleDelete}) => {
     <>
         {list.map((item) =>{
             return <>
-                    <div>
-                    <h2 style={{display :"inline-block", marginRight:" 10px" }}> {item.value}</h2>
-                    <button  onClick={()=> handleDelete(item.id)}>Delete</button>
-                    </div>
+                <div>
+                <h2 style={{display :"inline-block", marginRight:" 10px" }}> {item.value}</h2>
+                <button  onClick={()=> handleDelete(item.id)}>Delete</button>
+                </div>
             </>
         })}
     </>
